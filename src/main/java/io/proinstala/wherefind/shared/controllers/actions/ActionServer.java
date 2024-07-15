74e6e3edcdec283c07447ea393c981619d720f2e
Juan Manuel Soltero Sánchez <juanmanuelsolsan@gmail.com>
2024-07-15 14:04:00 +0200

    Agregado ActionServer.getRequestParameter para obtener los parámetros del request, en caso de no existir devuelve el valor por defecto que se le pasa

--- src/main/java/io/proinstala/wherefind/shared/controllers/actions/ActionServer.java
+++ src/main/java/io/proinstala/wherefind/shared/controllers/actions/ActionServer.java
@@ -1,8 +1,22 @@
 package io.proinstala.wherefind.shared.controllers.actions;
 
+import java.util.Map;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public record ActionServer(HttpServletRequest request, HttpServletResponse response) {
+
+    public String getRequestParameter(String name, String porDefecto)
+    {
+        Map<String, String[]> parametros = request.getParameterMap();
+        if (parametros.containsKey(name))
+        {
+            return parametros.get(name)[0];
+        }
+
+        return porDefecto;
+    }
+
 
 }
