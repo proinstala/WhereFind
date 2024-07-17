8a10e74bd62087d8d29d5ec72861fe49d4e7fd58
Juan Manuel Soltero Sánchez <juanmanuelsolsan@gmail.com>
2024-07-17 23:55:59 +0200

    Movidos los métodos getResponseOk, getResponseError y createResponseDTO a BaseService.java

--- src/main/java/io/proinstala/wherefind/identidad/services/IdentidadService.java
+++ src/main/java/io/proinstala/wherefind/identidad/services/IdentidadService.java
@@ -17,55 +17,6 @@ import io.proinstala.wherefind.shared.textos.LocaleApp;
  * Clase que maneja operaciones relacionadas con la identidad de los usuarios.
  */
 public class IdentidadService extends BaseService {
-    /**
-     * Crea un objeto ResponseDTO con los parámetros especificados.
-     *
-     * @param isError Indicador de error (1 si es un error, 0 si no lo es)
-     * @param isUrl   Indicador de URL (1 si la respuesta contiene una URL, 0 si no lo hace)
-     * @param mensaje Mensaje de respuesta
-     * @param objecto Información relacionada con la respuesta
-     * @return El objeto ResponseDTO creado
-     */
-    protected ResponseDTO createResponseDTO(int isError, int isUrl, String mensaje, Object objecto)
-    {
-        return new ResponseDTO(isError, isUrl, mensaje, objecto);
-    }
-
-    /**
-     * Devuelve un objeto ResponseDTO con error y mensaje especificados.
-     *
-     * @param mensaje Mensaje de respuesta
-     * @return El objeto ResponseDTO con error
-     */
-    protected ResponseDTO getResponseError(String mensaje)
-    {
-        return getResponseError(mensaje, new UserDTO());
-    }
-
-    /**
-     * Devuelve un objeto ResponseDTO con error, mensaje y objecto especificados.
-     *
-     * @param mensaje Mensaje de respuesta
-     * @param objecto Información relacionada con la respuesta
-     * @return El objeto ResponseDTO con error
-     */
-    protected ResponseDTO getResponseError(String mensaje, Object objecto)
-    {
-        return createResponseDTO(1, 0, mensaje, objecto);
-    }
-
-    /**
-     * Devuelve un objeto ResponseDTO con resultado OK y los parámetros especificados.
-     *
-     * @param mensaje Mensaje de respuesta
-     * @param objecto Información relacionada con la respuesta
-     * @param isUrl   Indicador de URL (1 si la respuesta contiene una URL, 0 si no lo hace)
-     * @return El objeto ResponseDTO con resultado OK
-     */
-    protected ResponseDTO getResponseOk(String mensaje, Object objecto, int isUrl)
-    {
-        return createResponseDTO(0, isUrl, mensaje, objecto);
-    }
 
     /**
      * Realiza el cierre de sesión del usuario.
--- src/main/java/io/proinstala/wherefind/shared/services/BaseService.java
+++ src/main/java/io/proinstala/wherefind/shared/services/BaseService.java
@@ -2,6 +2,9 @@ package io.proinstala.wherefind.shared.services;
 
 import java.io.IOException;
 import com.google.gson.GsonBuilder;
+
+import io.proinstala.wherefind.shared.dtos.ResponseDTO;
+import io.proinstala.wherefind.shared.dtos.UserDTO;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
@@ -43,4 +46,67 @@ public class BaseService {
     {
         return responseJson(response, new GsonBuilder().create().toJson(data));
     }
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Crea un objeto ResponseDTO con los parámetros especificados.
+     *
+     * @param isError Indicador de error (1 si es un error, 0 si no lo es)
+     * @param isUrl   Indicador de URL (1 si la respuesta contiene una URL, 0 si no lo hace)
+     * @param mensaje Mensaje de respuesta
+     * @param objecto Información relacionada con la respuesta
+     * @return El objeto ResponseDTO creado
+     */
+    protected ResponseDTO createResponseDTO(int isError, int isUrl, String mensaje, Object objecto)
+    {
+        return new ResponseDTO(isError, isUrl, mensaje, objecto);
+    }
+
+    /**
+     * Devuelve un objeto ResponseDTO con error y mensaje especificados.
+     *
+     * @param mensaje Mensaje de respuesta
+     * @return El objeto ResponseDTO con error
+     */
+    protected ResponseDTO getResponseError(String mensaje)
+    {
+        return getResponseError(mensaje, new UserDTO());
+    }
+
+    /**
+     * Devuelve un objeto ResponseDTO con error, mensaje y objecto especificados.
+     *
+     * @param mensaje Mensaje de respuesta
+     * @param objecto Información relacionada con la respuesta
+     * @return El objeto ResponseDTO con error
+     */
+    protected ResponseDTO getResponseError(String mensaje, Object objecto)
+    {
+        return createResponseDTO(1, 0, mensaje, objecto);
+    }
+
+    /**
+     * Devuelve un objeto ResponseDTO con resultado OK y los parámetros especificados.
+     *
+     * @param mensaje Mensaje de respuesta
+     * @param objecto Información relacionada con la respuesta
+     * @param isUrl   Indicador de URL (1 si la respuesta contiene una URL, 0 si no lo hace)
+     * @return El objeto ResponseDTO con resultado OK
+     */
+    protected ResponseDTO getResponseOk(String mensaje, Object objecto, int isUrl)
+    {
+        return createResponseDTO(0, isUrl, mensaje, objecto);
+    }
+
+
 }
