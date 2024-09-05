7ef5e026930df5eec00f077fdb9e05f98e4ec51b
Juan Manuel Soltero Sánchez <juanmanuelsolsan@gmail.com>
2024-09-05 19:13:00 +0200

    Modificado iconos de la cabecera para que apunten al panel de dirección y al de adminstración o panel de control

--- src/main/webapp/App/js/cabecera.js
+++ src/main/webapp/App/js/cabecera.js
@@ -3,18 +3,32 @@ $(document).ready(function () {
 
     const btnEditarUsuario = document.querySelector('#btnEditarUsuario');
     const btnLogout = document.querySelector('#btnLogout');
-    
+
     const btnDireccion = document.querySelector('#btnDireccion');
+    const btnConfig = document.querySelector('#btnConfig-user');
+    const btnConfigAdmin = document.querySelector('#btnConfig-admin');
+
+
 
     btnEditarUsuario.addEventListener('click', () => {
         window.location.href = 'account/modificar';
     });
-    
+
     btnLogout.addEventListener('click', () => {
         window.location.href = 'api/identidad/logout';
     });
-    
+
     btnDireccion.addEventListener('click', () => {
-        window.location.href = ('direccion/adminDireccion');
+        window.location.href = ('direccion');
     });
+
+    btnConfig?.addEventListener('click', () => {
+        window.location.href = ('account');
+    });
+
+    btnConfigAdmin?.addEventListener('click', () => {
+        window.location.href = ('admin');
+    });
+
+
 });
